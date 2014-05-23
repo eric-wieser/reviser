80f57edec4dc20f9201823cadde8b2558d6edf9e
Eric Wieser <wieser.eric@gmail.com>
2014-05-23 12:11:19 +0100

    Log text

--- api.js
+++ api.js
@@ -52,6 +52,8 @@ PageSlice.prototype.render = function(canvas) {
   canvas.height = newVP.height;
   canvas.width = newVP.width;
 
+  this.page.getTextContent().then(console.info.bind(console));
+
   if(this._lastCanvas && this._lastCanvas.width == canvas.width && this._lastCanvas.height == canvas.height) {
     context.drawImage(this._lastCanvas, 0, 0);
   }
